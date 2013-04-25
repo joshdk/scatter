a1afabca622e84decbfa7b7224910b1c618750c8
Josh Komoroske <jdkomo.dev@gmail.com>
2013-04-25 17:45:17 -0400

    Moved code into the master and slave functions

--- src/main.c
+++ src/main.c
@@ -24,6 +24,52 @@ int mpi_master(size_t ranks, size_t rank, void * data){
 
 
 int mpi_slave(size_t ranks, size_t rank, void * data){
+	char * module = ((void **)data)[0];
+	size_t iterations = *(size_t *)((void **)data)[1];
+	printf("rank: %zu | module: %s iters: %zu\n", rank, module, iterations);
+
+
+	hash_ctx hctx;
+	hash_init(&hctx);
+
+	if(hash_load(&hctx, module) != 0){
+		fprintf(stderr, "scatter: error: Failed to load module `%s'\n", module);
+		return 1;
+	}
+
+	size_t pass_size = 256;
+	size_t pass_length = 0;
+	char * pass = calloc(pass_size, sizeof(char));
+
+	size_t hash_size;
+	hctx.info(&hash_size);
+	char * hash = calloc(hash_size, sizeof(char));
+
+	pass_ctx pctx;
+	pass_init(&pctx, 3);
+	pass_load_int(&pctx, rank-1);
+
+	pass_ctx pstp;
+	pass_init(&pstp, 3);
+	pass_load_int(&pstp, ranks-1);
+
+	for(size_t n=0; n<iterations; n++){
+		pass_blit(&pctx, "abc", pass, &pass_length);
+		// printf("pass: %s\n", pass);
+		hctx.hash(pass, pass_length, hash, &hash_size);
+		// print_hex(hash, hash_size);
+		pass_step(&pctx, &pstp);
+	}
+
+	printf("rank: %zu | pass: %s\n", rank, pass);
+
+	pass_fini(&pctx);
+	pass_fini(&pstp);
+
+	free(pass);
+	free(hash);
+
+	hash_fini(&hctx);
 
 	return 0;
 }
@@ -46,52 +92,22 @@ int mpi_main(size_t ranks, size_t rank, size_t argc, char **argv){
 	module = argv[1];
 
 	size_t iterations = 100;
-
 	if(argc >= 3){
 		iterations = atoi(argv[2]);
 	}
 
-	hash_ctx hctx;
-	hash_init(&hctx);
+	void ** data = NULL;
+	data = malloc(sizeof(void *) * 2);
+	data[0] = module;
+	data[1] = &iterations;
 
-	if(hash_load(&hctx, module) != 0){
-		fprintf(stderr, "scatter: error: Failed to load module `%s'\n", module);
-		return 1;
+	if(rank == 0){
+		mpi_master(ranks, rank, NULL);
+
+	}else{
+		mpi_slave(ranks, rank, data);
+
 	}
-
-	size_t pass_size = 256;
-	size_t pass_length = 0;
-	char * pass = calloc(pass_size, sizeof(char));
-
-	size_t hash_size;
-	hctx.info(&hash_size);
-	char * hash = calloc(hash_size, sizeof(char));
-
-	pass_ctx pctx;
-	pass_init(&pctx, 3);
-	pass_load_int(&pctx, rank);
-
-	pass_ctx pstp;
-	pass_init(&pstp, 3);
-	pass_load_int(&pstp, ranks);
-
-	for(size_t n=0; n<iterations; n++){
-		pass_blit(&pctx, "abc", pass, &pass_length);
-		// printf("pass: %s\n", pass);
-		hctx.hash(pass, pass_length, hash, &hash_size);
-		// print_hex(hash, hash_size);
-		pass_step(&pctx, &pstp);
-	}
-
-	printf("rank: %zu | pass: %s\n", rank, pass);
-
-	pass_fini(&pctx);
-	pass_fini(&pstp);
-
-	free(pass);
-	free(hash);
-
-	hash_fini(&hctx);
 
 	return 0;
 }
