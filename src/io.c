e3b84b49924ecc6ae9e21f87a264a2628d3acb8b
Josh Komoroske <jdkomo.dev@gmail.com>
2013-05-02 22:09:59 -0400

    Added functions for sending & recieving hashes

--- src/io.c
+++ src/io.c
@@ -31,4 +31,50 @@ int mpi_recv_hash_type(char ** type){
 	return 0;
 }
 
+
+int mpi_isend_hashes(char ** data, size_t size, size_t nmem, int rank){
+	size_t buffer_size = nmem * size;
+	char * buffer = calloc(nmem * size, sizeof(char));
+
+	for(size_t i=0; i<nmem; i++){
+		memcpy(buffer + i*size, data[i], size);
+	}
+
+	MPI_Request request;
+
+	MPI_Isend(buffer, buffer_size, MPI_CHAR, rank, 1, MPI_COMM_WORLD, &request);
+
+	return 0;
+}
+
+
+int mpi_recv_hashes(char *** data, size_t size, size_t * nmem){
+	MPI_Status status;
+	int buffer_size = 0;
+
+	MPI_Probe(0, 1, MPI_COMM_WORLD, &status);
+	MPI_Get_count(&status, MPI_CHAR, &buffer_size);
+	assert(buffer_size % size == 0);
+
+	char * buffer = NULL;
+	buffer = calloc(buffer_size, sizeof(char));
+
+	MPI_Recv(buffer, buffer_size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
+
+	size_t hashes_size = buffer_size / size;
+	size_t hashes_length = 0;
+	char ** hashes = malloc(sizeof(*hashes) * hashes_size);
+
+	for(size_t i=0; i<hashes_size; i++){
+		hashes[i] = malloc(sizeof(char) * size);
+		memcpy(hashes[i], buffer + i*size, size);
+	}
+
+	free(buffer);
+
+	*data = hashes;
+	*nmem = hashes_size;
+	return 0;
+}
+
 #endif
--- src/io.h
+++ src/io.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 
 
@@ -12,8 +13,8 @@
 int mpi_isend_hash_type(const char * type, int rank);
 int mpi_recv_hash_type(char ** type);
 
-int mpi_isend_hash(char * data, size_t size, int rank);
-int mpi_recv_hash(char ** data);
+int mpi_isend_hashes(char ** data, size_t nmem, size_t size, int rank);
+int mpi_recv_hashes(char *** data, size_t size, size_t * nmem);
 
 int mpi_isend_start(int rank);
 int mpi_isend_stop(int rank);
